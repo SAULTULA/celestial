af2895ee970971bb2cc47969a6fd98237898d247
SAULTULA <67132634+SAULTULA@users.noreply.github.com>
2026-09-14 18:29:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'radio-hits20-v4';
+const urlsToCache = [
+  './index.html',
+  './styles.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
